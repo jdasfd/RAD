f0bcce9e0cd245f1f622d898220cfa2b154f1728
Yuqian Jiang <597174820@qq.com>
2023-08-16 16:28:54 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,11 @@
 CPAN_MIRROR=https://mirrors.ustc.edu.cn/CPAN/
 NO_TEST=--notest
 
-curl -L https://cpanmin.us |
-    $HOME/share/Perl/bin/perl - -v --mirror-only --mirror $CPAN_MIRROR App::cpanminus
+hash cpanm 2>/dev/null || {
+    echo >&2 "Install cpanm";
+    curl -L https://cpanmin.us |
+        $HOME/share/Perl/bin/perl - -v --mirror-only --mirror $CPAN_MIRROR App::cpanminus
+}
 
 cpanm --mirror-only --mirror $CPAN_MIRROR $NO_TEST Math::BigFloat IO::Tee Math::BigFloat Getopt::Long
 cpanm --mirror-only --mirror $CPAN_MIRROR $NO_TEST Bio::SearchIO Bio::Seq Bio::SeqIO
